3abb8f8c9f50416faab606cc81281d4168abf880
realcaldeira <mrrealcaldeira@gmail.com>
2021-11-13 18:07:35 -0300

    inicio da tela home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../../providers/auth';
 
 import {
   Container,
-  Title
+  Title,
+  ContainerSearch,
+  InputSearch,
+  ButtonSearch,
+  ContainerOptions,
+  ButtonOptions,
+  TitleOptions
 } from './styles';
 
 
 export function Home() {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
 
+  const { token, list } = useContext(AuthContext);
+
+  function handleSearch() {
+    // Alert.alert(search);
+
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+    };
+
+    axios.get(
+      `https://farmappapi.herokuapp.com/api/Produto?IdTipoProduto=2&Busca=${search}`,
+      config
+    ).then((json) =>
+      json.status === 200
+        ?
+        // console.log(json.data[0])
+        setData(json.data[0])
+        : '')
+      .catch(console.log);
+  }
   return (
     <Container>
-      <Title>HOME</Title>
+      <ContainerSearch>
+        <InputSearch
+          placeholder="Busca por produto"
+          placeholderTextColor='white'
+          onChangeText={setSearch}
+        />
+        <ButtonSearch
+          onPress={handleSearch}
+        >
+          <Title>Pesquisar</Title>
+        </ButtonSearch>
+      </ContainerSearch>
+
+      <ContainerOptions>
+        <ButtonOptions>
+          <TitleOptions>distância</TitleOptions>
+        </ButtonOptions>
+
+        <ButtonOptions>
+          <TitleOptions>preço</TitleOptions>
+        </ButtonOptions>
+      </ContainerOptions>
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,57 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   height: 100%;
-  background-color: ${({ theme })=> theme.colors.background_primary};
-  justify-content: center;
-  align-content: center;
-  
+  background-color: ${({ theme }) => theme.colors.background_primary};
+
 `;
 
 export const Title = styled.Text`
+  color: white;
+`;
+
+export const ContainerSearch = styled.View`
+  height: 10%;
+  padding-top: ${RFValue(30)}px;
+  /* background-color: red; */
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0 12px;
+`;
+
+export const InputSearch = styled.TextInput`
+  width: ${RFValue(255)}px;
+  height:  ${RFValue(37)}px;
+  
   text-align: center;
+  color: white;
+  background: #525F8F;
+  border-radius: 10px;
+`;
+
+export const ButtonSearch = styled.TouchableOpacity`
+  width: ${RFValue(104)}px;
+  height:  ${RFValue(37)}px;
+  
+  justify-content: center;
+  align-items: center;
+
+  background: #525F8F;
+  border-radius: 10px;
+`;
+
+export const ContainerOptions = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: ${RFValue(10)}px ${RFValue(50)}px;
+`;
+export const ButtonOptions = styled.TouchableOpacity`
+  background-color: red;
+  align-items: center;
+  width: ${RFValue(125)}px;
+  height: ${RFValue(20)}px;
+`;
+export const TitleOptions = styled.Text`
+  color: black;
 `;
